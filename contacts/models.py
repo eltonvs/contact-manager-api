1807fe5292586736d1e3153c02302ac3b1f4e638
Elton Viana <eltonvs@outlook.com>
2018-10-20 23:22:07 -0300

    Create Initial Contact Model Implementation

--- contacts/models.py
+++ contacts/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Contact(models.Model):
-    pass
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    date_of_birth = models.DateField(editable=True)
+
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
