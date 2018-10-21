f8e1f46ecd78a70f1668c7860d8b4ea36cce6486
Elton Viana <eltonvs@outlook.com>
2018-10-21 16:55:30 -0300

    Create a views tests package

--- contacts/tests/views/test_contact.py
+++ contacts/tests/views/test_contact.py
@@ -1,154 +1,12 @@
-import json
-from datetime import date
 from django.urls import reverse
 from rest_framework import status
 
-from rest_framework.test import APITestCase, APIClient
-
-from contacts.models import Contact, PhoneNumber
+from contacts.models import Contact
 from contacts.serializers import ContactSerializer
+from contacts.tests.views.base_contact_view_test import BaseContactViewTest
 
 
-class BaseViewTest(APITestCase):
-    client = APIClient()
-
-    @staticmethod
-    def insert_contact(first_name, last_name, date_of_birth, phone_numbers):
-        """
-        Create a contact and store on database
-        :param first_name:
-        :param last_name:
-        :param date_of_birth:
-        :param phone_numbers:
-        """
-        if first_name != '' and last_name != '' and date_of_birth and phone_numbers:
-            contact = Contact.objects.create(first_name=first_name, last_name=last_name, date_of_birth=date_of_birth)
-            for phone_number in phone_numbers:
-                PhoneNumber.objects.create(
-                    contact=contact, phone=phone_number['phone'], primary=phone_number.get('primary', False)
-                )
-
-    def setUp(self):
-        # Populate database
-        self.insert_contact('Elton', 'John', date(1947, 3, 25), [{'phone': '+44 7911 123456', 'primary': True}])
-        self.insert_contact('Elvis', 'Presley', date(1935, 1, 8), [{'phone': '+1 123 456 7890', 'primary': True}])
-        self.insert_contact(
-            'Marilyn', 'Monroe', date(1926, 6, 1),
-            [{'phone': '+1 000 111 2222', 'primary': True}, {'phone': '+1 321 654 0987'}]
-        )
-        # Default Values
-        self.valid_contact_id = 1
-        self.valid_contact_id_with_multiple_phones = 3
-        self.invalid_contact_id = 42
-        self.current_version = 'v1'
-        # Default tests data
-        self.valid_contact_data = {
-            'first_name': 'John',
-            'last_name': 'Doe',
-            'date_of_birth': '1990-10-20',
-        }
-        self.empty_contact_data = {
-            'first_name': '',
-            'last_name': '',
-            'date_of_birth': '',
-        }
-        self.valid_phone_data = {
-            'phone_numbers': [
-                {
-                    'phone': '+1 202 555 0104',
-                    'primary': True
-                },
-            ]
-        }
-        self.valid_multiple_phone_data = {
-            'phone_numbers': [
-                {
-                    'phone': '+1 202 555 0104',
-                    'primary': True
-                },
-                {
-                    'phone': '+2 123 456 7890',
-                    'primary': False
-                },
-                {
-                    'phone': '+55 999 999 999',
-                    'primary': False
-                },
-            ]
-        }
-        self.invalid_multiple_phone_data = {
-            'phone_numbers': [
-                {
-                    'phone': '+1 202 555 0104',
-                    'primary': True
-                },
-                {
-                    'phone': '+2 123 456 7890',
-                    'primary': False
-                },
-                {
-                    'phone': '+55 999 999 999',
-                    'primary': False
-                },
-                {
-                    'phone': ''
-                }
-            ]
-        }
-        self.empty_phone_data = {
-            'phone_numbers': []
-        }
-        self.phone_data_with_empty_phone = {
-            'phone_numbers': [{'phone': ''}]
-        }
-
-    def create_contact(self, data):
-        """
-        Perform a POST request to create a new contact
-        :param data:
-        :return:
-        """
-        return self.client.post(
-            reverse('contacts-list', kwargs={'version': self.current_version}),
-            data=json.dumps(data),
-            content_type='application/json'
-        )
-
-    def update_contact(self, contact_id, new_data):
-        """
-        Perform a PUT request to update an existing contact
-        :param contact_id:
-        :param new_data:
-        :return:
-        """
-        return self.client.put(
-            reverse('contact-details', kwargs={'version': self.current_version, 'contact_id': contact_id}),
-            data=json.dumps(new_data),
-            content_type='application/json'
-        )
-
-    def fetch_contact(self, contact_id):
-        """
-        Perform a GET request to retrieve an existing contact by your id
-        :param contact_id:
-        :return:
-        """
-        return self.client.get(
-            reverse('contact-details', kwargs={'version': self.current_version, 'contact_id': contact_id})
-        )
-
-    def remove_contact(self, contact_id):
-        """
-        Perform a DELETE request to remove an existing contact by your id
-        :param contact_id:
-        :return:
-        """
-        return self.client.delete(
-            reverse('contact-details', kwargs={'version': self.current_version, 'contact_id': contact_id})
-        )
-
-
-class GetAllContactsTest(BaseViewTest):
+class GetAllContactsTest(BaseContactViewTest):
     def test_get_all_contacts(self):
         """
         This ensures that all contacts added in the setUp method
@@ -165,7 +23,7 @@ class GetAllContactsTest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-class GetAContactTest(BaseViewTest):
+class GetAContactTest(BaseContactViewTest):
     def test_get_a_contact(self):
         """
         This test ensures that a single contact (with a given id) can be retrieved
@@ -205,7 +63,7 @@ class GetAContactTest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-class CreateContactTest(BaseViewTest):
+class CreateContactTest(BaseContactViewTest):
     def test_create_a_contact(self):
         """
         This test ensures that a single contact can be created
@@ -304,7 +162,7 @@ class CreateContactTest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class UpdateContactTest(BaseViewTest):
+class UpdateContactTest(BaseContactViewTest):
     def test_update_a_contact(self):
         """
         This test ensures that a single contact can be updated
@@ -338,7 +196,7 @@ class UpdateContactTest(BaseViewTest):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
-class RemoveContactTest(BaseViewTest):
+class RemoveContactTest(BaseContactViewTest):
     def test_remove_a_contact(self):
         """
         This test ensures that a single contact can be removed
