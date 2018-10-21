280e65cdfc21fa445554bc3e421a095c12de0ef3
Elton Viana <eltonvs@outlook.com>
2018-10-21 00:03:01 -0300

    Create Contact Views Tests
    - Test retrieval from all contacts
    - Test retrieval from a contact
    - Test retrieval from a nonexistent contact
    - Test contact creation
    - Test empty contact creation
    - Test contact update
    - Test contact update to an empty contact
    - Test contact removal
    - Test nonexistent contact removal

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,9 +1,11 @@
 import json
 from datetime import date
 from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
 from contacts.models import Contact
+from contacts.serializers import ContactSerializer
 
 
 # Models Tests
@@ -106,3 +108,114 @@ class BaseViewTest(APITestCase):
         return self.client.delete(
             reverse('contact-details', kwargs={'version': self.current_version, 'contact_id': contact_id})
         )
+
+
+class GetAllContactsTest(BaseViewTest):
+    def test_get_all_contacts(self):
+        """
+        This ensures that all contacts added in the setUp method
+        exist when we make a GET request to the contacts/ endpoint
+        """
+        # Retrieve response from API
+        response = self.client.get(reverse('contacts-list', kwargs={'version': self.current_version}))
+
+        # Fetch data from database
+        expected = Contact.objects.all()
+        serialized = ContactSerializer(expected, many=True)
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetAContactTest(BaseViewTest):
+    def test_get_a_contact(self):
+        """
+        This test ensures that a single contact (with a given id) can be retrieved
+        """
+        # Retrieve response from API
+        response = self.fetch_contact(self.valid_contact_id)
+
+        # Fetch data from database
+        expected = Contact.objects.get(pk=self.valid_contact_id)
+        serialized = ContactSerializer(expected)
+
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_a_nonexistent_contact(self):
+        """
+        This test ensures that a nonexistent contact cannot be retrieved
+        """
+        # Retrieve response from API
+        response = self.fetch_contact(self.invalid_contact_id)
+
+        self.assertTrue('not exist' in response.data['message'])
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class CreateContactTest(BaseViewTest):
+    def test_create_a_contact(self):
+        """
+        This test ensures that a single contact can be created
+        """
+        # Use the API endpoint to create a new contact
+        response = self.create_contact(self.valid_contact_data)
+
+        self.assertEqual(response.data, self.valid_contact_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_an_empty_contact(self):
+        """
+        This test ensures that an empty contact cannot be created
+        """
+        # Use the API endpoint to create a new contact
+        response = self.create_contact(self.empty_contact_data)
+
+        self.assertTrue('required' in response.data['message'])
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class UpdateContactTest(BaseViewTest):
+    def test_update_a_contact(self):
+        """
+        This test ensures that a single contact can be updated
+        """
+        # Use the API endpoint to update a contact
+        response = self.update_contact(contact_id=2, new_data=self.valid_contact_data)
+
+        self.assertEqual(response.data, self.valid_contact_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_a_contact_with_empty_values(self):
+        """
+        This test ensures that a single contact cannot be updated with empty data
+        """
+        # Use the API endpoint to update a contact
+        response = self.update_contact(contact_id=2, new_data=self.empty_contact_data)
+
+        self.assertTrue('required' in response.data['message'])
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class RemoveContactTest(BaseViewTest):
+    def test_remove_a_contact(self):
+        """
+        This test ensures that a single contact can be removed
+        """
+        # Use the API endpoint to remove a contact
+        response = self.remove_contact(1)
+        try:
+            Contact.objects.get(pk=1)
+            self.fail()
+        except Contact.DoesNotExist:
+            pass
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_remove_an_nonexistent_contact(self):
+        """
+        This test ensures that a nonexistent contact cannot be removed
+        """
+        # Use the API endpoint to remove a contact
+        response = self.remove_contact(self.invalid_contact_id)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
