2c6da0e77baf63550eff4d452aee04fc7e3db7a0
Elton Viana <eltonvs@outlook.com>
2018-10-23 23:00:01 -0300

    Use serializer to validate fields
    - Use django shortcuts to a more readable code

--- contacts/views/emails.py
+++ contacts/views/emails.py
@@ -1,8 +1,7 @@
-from django.db import IntegrityError
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from contacts.decorators import validate_flat_email_data
 from contacts.models import EmailField, Contact
 from contacts.serializers import EmailFieldSerializer
 
@@ -16,59 +15,34 @@ class ListEmailsView(generics.ListCreateAPIView):
     def get_queryset(self):
         return EmailField.objects.filter(contact_id=self.kwargs['contact_id'])
 
-    @validate_flat_email_data
     def post(self, request, *args, **kwargs):
-        try:
-            requested_contact = Contact.objects.get(pk=kwargs['contact_id'])
-            inserted_email = EmailField.objects.create(contact=requested_contact, email=request.data['email'])
-            return Response(self.serializer_class(inserted_email).data, status=status.HTTP_201_CREATED)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        except IntegrityError:
-            return Response(data={'message': 'This email is already registered'}, status=status.HTTP_409_CONFLICT)
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        requested_contact = get_object_or_404(Contact, pk=kwargs['contact_id'])
+        inserted_email = EmailField.objects.create(contact=requested_contact, **serializer.validated_data)
+        return Response(self.serializer_class(inserted_email).data, status=status.HTTP_201_CREATED)
 
 
 class EmailDetailsView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = EmailFieldSerializer
 
     def get(self, request, *args, **kwargs):
-        try:
-            requested_contact = Contact.objects.get(pk=kwargs['contact_id'])
-            retrieved_email = requested_contact.emails.get(email=kwargs['email'])
-            return Response(self.serializer_class(retrieved_email).data)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        except EmailField.DoesNotExist:
-            return Response(data={'message': 'The requested email does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        retrieved_email = get_object_or_404(EmailField, contact_id=kwargs['contact_id'], email=kwargs['email'])
+        return Response(self.serializer_class(retrieved_email).data)
 
-    @validate_flat_email_data
     def put(self, request, *args, **kwargs):
-        try:
-            requested_contact = Contact.objects.get(pk=kwargs['contact_id'])
-            original_email = requested_contact.emails.get(email=kwargs['email'])
-            serializer = self.serializer_class()
-            updated_contact = serializer.update(original_email, request.data)
-            return Response(self.serializer_class(updated_contact).data)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        except EmailField.DoesNotExist:
-            return Response(data={'message': 'The requested email does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        except IntegrityError:
-            return Response(
-                data={'message': 'This email is already registered'},
-                status=status.HTTP_409_CONFLICT
-            )
+        original_email = get_object_or_404(EmailField, contact_id=kwargs['contact_id'], email=kwargs['email'])
+
+        serializer = self.serializer_class(original_email, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
-        try:
-            requested_contact = Contact.objects.get(pk=kwargs['contact_id'])
-            if requested_contact.emails.count() > 1:
-                requested_email = requested_contact.emails.get(email=kwargs['email'])
-                requested_email.delete()
-                return Response(status=status.HTTP_204_NO_CONTENT)
-
-            return Response(data={'message': 'This email cannot be deleted'}, status=status.HTTP_400_BAD_REQUEST)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
-        except EmailField.DoesNotExist:
-            return Response(data={'message': 'The requested email does not exist'}, status=status.HTTP_404_NOT_FOUND)
+        requested_contact = get_object_or_404(Contact, pk=kwargs['contact_id'])
+        if requested_contact.emails.count() > 1:
+            requested_email = get_object_or_404(EmailField, email=kwargs['email'])
+            requested_email.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(data={'email': ['This email cannot be deleted']}, status=status.HTTP_400_BAD_REQUEST)
