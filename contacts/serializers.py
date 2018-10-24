2c842e181f28ac1f1786bfda381bcb14a13c875e
Elton Viana <eltonvs@outlook.com>
2018-10-24 11:01:01 -0300

    Update contacts view and serializer to use django defaults
    - Remove unused decorators and validators

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction, IntegrityError
 from rest_framework import serializers
 
 from contacts.models import Contact, PhoneNumber, EmailField, AddressField
@@ -18,14 +19,56 @@ class EmailFieldSerializer(serializers.ModelSerializer):
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = AddressField
-        fields = ('address', 'city', 'state', 'country', 'zip_code')
+        fields = ('id', 'address', 'city', 'state', 'country', 'zip_code')
 
 
 class ContactSerializer(serializers.ModelSerializer):
-    phone_numbers = serializers.SlugRelatedField(many=True, queryset=PhoneNumber.objects.all(), slug_field='phone')
-    emails = serializers.SlugRelatedField(many=True, queryset=[], slug_field='email')
-    addresses = AddressSerializer(many=True)
+    phone_numbers = serializers.SlugRelatedField(many=True, read_only=True, slug_field='phone')
+    emails = serializers.SlugRelatedField(many=True, read_only=True, slug_field='email')
+    addresses = AddressSerializer(many=True, required=False)
 
     class Meta:
         model = Contact
-        fields = ('first_name', 'last_name', 'date_of_birth', 'phone_numbers', 'emails', 'addresses')
+        fields = ('id', 'first_name', 'last_name', 'date_of_birth', 'phone_numbers', 'emails', 'addresses')
+
+
+class ContactNestedSerializer(serializers.ModelSerializer):
+    phone_numbers = PhoneNumberSerializer(many=True, required=True)
+    emails = EmailFieldSerializer(many=True, required=True)
+    addresses = AddressSerializer(many=True, required=False)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        phone_numbers_data = validated_data.pop('phone_numbers')
+        emails_data = validated_data.pop('emails')
+        addresses_data = validated_data.pop('addresses')
+
+        created_contact = self.Meta.model.objects.create(**validated_data)
+
+        if not phone_numbers_data:
+            raise serializers.ValidationError({'phone_numbers': ['This field is required.']})
+        for phone_data in phone_numbers_data:
+            phone_serializer = PhoneNumberSerializer(PhoneNumber(contact=created_contact), data=phone_data)
+            phone_serializer.is_valid(raise_exception=True)
+            phone_serializer.save()
+
+        if not emails_data:
+            raise serializers.ValidationError({'emails': ['This field is required.']})
+        for email_data in emails_data:
+            email_serializer = EmailFieldSerializer(EmailField(contact=created_contact), data=email_data)
+            email_serializer.is_valid(raise_exception=True)
+            email_serializer.save()
+
+        for address_data in addresses_data:
+            address_serializer = AddressSerializer(AddressField(contact=created_contact), data=address_data)
+            address_serializer.is_valid(raise_exception=True)
+            try:
+                address_serializer.save()
+            except IntegrityError:
+                raise serializers.ValidationError({'addresses': 'This field is already registered'})
+
+        return created_contact
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
--- contacts/views/contacts.py
+++ contacts/views/contacts.py
@@ -1,11 +1,9 @@
-from django.db import transaction, IntegrityError
+from django.db import transaction
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-from contacts.decorators import validate_contact_data, validate_phone_number_data, validate_emails_data, \
-    validate_address_data
-from contacts.models import Contact, PhoneNumber, EmailField, AddressField
-from contacts.serializers import ContactSerializer
+from contacts.models import Contact
+from contacts.serializers import ContactSerializer, ContactNestedSerializer
 
 
 class ListContactsView(generics.ListCreateAPIView):
@@ -15,64 +13,20 @@ class ListContactsView(generics.ListCreateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
-    @validate_contact_data
-    @validate_phone_number_data
-    @validate_emails_data
-    @validate_address_data
+    @transaction.atomic
     def post(self, request, *args, **kwargs):
-        try:
-            with transaction.atomic():
-                created_contact = Contact.objects.create(
-                    first_name=request.data['first_name'],
-                    last_name=request.data['last_name'],
-                    date_of_birth=request.data['date_of_birth']
-                )
-                for phone_number in request.data['phone_numbers']:
-                    PhoneNumber.objects.create(contact=created_contact, phone=phone_number)
-                for email in request.data['emails']:
-                    EmailField.objects.create(contact=created_contact, email=email)
-                for address in request.data['addresses']:
-                    AddressField.objects.create(
-                        contact=created_contact,
-                        address=address['address'],
-                        city=address['city'],
-                        state=address['state'],
-                        country=address['country'],
-                        zip_code=address['zip_code']
-                    )
-                return Response(data=self.serializer_class(created_contact).data, status=status.HTTP_201_CREATED)
-        except IntegrityError:
-            return Response(
-                data={'message': 'The contact is not valid. There are attributes with duplicated values'},
-                status=status.HTTP_409_CONFLICT
-            )
+        request.data['phone_numbers'] = list(map(lambda x: {'phone': x}, request.data.get('phone_numbers', [])))
+        request.data['emails'] = list(map(lambda x: {'email': x}, request.data.get('emails', [])))
+
+        serializer = ContactNestedSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        created_contact = serializer.create(serializer.validated_data)
+
+        return Response(data=self.serializer_class(created_contact).data, status=status.HTTP_201_CREATED)
 
 
 class ContactDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-
-    def get(self, request, *args, **kwargs):
-        try:
-            retrieved_contact = self.queryset.get(pk=kwargs['contact_id'])
-            return Response(self.serializer_class(retrieved_contact).data)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-    @validate_contact_data
-    def put(self, request, *args, **kwargs):
-        try:
-            original_contact = self.queryset.get(pk=kwargs['contact_id'])
-            serializer = self.serializer_class()
-            updated_contact = serializer.update(original_contact, request.data)
-            return Response(self.serializer_class(updated_contact).data)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, *args, **kwargs):
-        try:
-            requested_contact = self.queryset.get(pk=kwargs['contact_id'])
-            requested_contact.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Contact.DoesNotExist:
-            return Response(data={'message': 'The requested contact does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    lookup_url_kwarg = 'contact_id'
