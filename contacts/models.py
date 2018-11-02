f5ac73aa5938f0e6d7f1d70fcf989930cd86252c
Elton Viana <eltonvs@outlook.com>
2018-11-02 00:47:31 -0300

    Add model constraints to do not accept invalid phone numbers

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
 
@@ -12,7 +13,8 @@ class Contact(models.Model):
 
 class PhoneNumber(models.Model):
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='phone_numbers')
-    phone = models.CharField(max_length=100, unique=True)
+    phone_regex = RegexValidator(regex=r'^\+(?:[0-9] ?){6,14}[0-9]$')
+    phone = models.CharField(validators=[phone_regex], max_length=20, unique=True)
 
     def __str__(self):
         return self.phone
